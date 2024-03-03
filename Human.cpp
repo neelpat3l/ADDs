97285b7cdd87b94980bb4efb54dbe01092b546e6
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2024-03-03 23:25:50 +1030

    Prac1

--- Human.cpp
+++ Human.cpp
@@ -1,8 +1,8 @@
 #include "Human.h"
 #include <iostream>
-
+//Constructor
 Human::Human(const std::string& name) : Player(name) {}
-
+//Prompt the input message to the human player
 char Human::makeMove() {
     char move;
     std::cout << "Enter move(R, P, S): ";
--- Referee.cpp
+++ Referee.cpp
@@ -2,16 +2,16 @@
 #include <iostream>
 
 Referee::Referee() {}
-
+//Associate the move with player Player 1 as human and player 2 as computer completed in Main.cpp
 Player* Referee::refGame(Player* player1, Player* player2) {
     char move1 = player1->makeMove();
     char move2 = player2->makeMove();
-
+// Print It's a Tie. when move 1 is the same as move 2 
     if (move1 == move2) {
         std::cout << "It's a Tie." << std::endl;
         return nullptr;
     }
-
+// Compare the results of the moves
     if ((move1 == 'R' && move2 == 'S') || (move1 == 'S' && move2 == 'P') || (move1 == 'P' && move2 == 'R')) {
         std::cout << player1->getName() << " Wins." << std::endl;
         return player1;
